39ec385eb218c8aa32164f5c5a7ba8c9c2e07725
lizhao local <hlwords@163.com>
2017-12-13 23:36:06 -0500

    add photo with Picasso

--- app/src/main/java/com/example/lizhao/myfirstapplication/Main2Activity.java
+++ app/src/main/java/com/example/lizhao/myfirstapplication/Main2Activity.java
@@ -1,15 +1,54 @@
 package com.example.lizhao.myfirstapplication;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+
+public class Main2Activity extends AppCompatActivity implements OnItemSelectedListener{
 
     private TextView mTextMessage;
+    private Spinner spMainSelectCategory;
+    private Spinner idSpinner;
+    private EditText inputID;
+    private TextView tvMainSelectedCate;
+    private TextView tv2;
+    private TextView tv3;
+    private ImageView tou;
+    private ArrayList<Player> categoryList = new ArrayList<Player>();
+    private ArrayList<String> nameList = new ArrayList<String>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +78,65 @@ public class Main2Activity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        spMainSelectCategory = (Spinner) findViewById(R.id.spMainSelectCategory);
+        tvMainSelectedCate = (TextView) findViewById(R.id.tvMainSelectedCate);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+        tou = (ImageView) findViewById(R.id.tou);
+        //tou.setImageBitmap(returnBitMap("https://cdn.sofifa.org/48/18/players/20801.png"));
+
+
+
+        List<String[]> list = new ArrayList<String[]>();
+        String next[] = {};
+        try {
+            InputStreamReader csvStreamReader = new InputStreamReader(
+                    Main2Activity.this.getAssets().open(
+                            "PlayerPersonalData2.0.csv"));
+
+            CSVReader reader = new CSVReader(csvStreamReader);
+            for (;;) {
+                next = reader.readNext();
+                if (next != null) {
+                    list.add(next);
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < list.size(); i++) {
+            categoryList.add(new Player(Integer.parseInt(list.get(i)[0]), list.get(i)[2], list.get(i)[5], Integer.parseInt(list.get(i)[3]), list.get(i)[4]));
+            nameList.add(list.get(i)[2]);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, nameList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spMainSelectCategory.setAdapter(adapter);
+        spMainSelectCategory.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        tvMainSelectedCate.setText("Name: "
+                + categoryList.get(arg2).getName());
+        tv2.setText("Country: "
+                + categoryList.get(arg2).getCountry());
+        tv3.setText("Age: "
+                + categoryList.get(arg2).getAge());
+        Picasso.with(tou.getContext()).load(categoryList.get(arg2).getPhoto()).into(tou);
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
     }
 
 }
