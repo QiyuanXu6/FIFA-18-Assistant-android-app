c77a0b3eaf91e1ca64185aa5ebc456a3f7252aec
Hai Cao <imcaohai@gmail.com>
2017-12-17 20:23:10 -0500

    Added coverage report

--- app/src/test/java/com/fifa/FifaDataControllerTest.java
+++ app/src/test/java/com/fifa/FifaDataControllerTest.java
@@ -2,6 +2,10 @@ package com.fifa;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,7 +14,17 @@ import static org.junit.Assert.*;
 public class FifaDataControllerTest {
     @Test
     public void startReading() throws Exception {
-
+        PlayerData pd = new PlayerData();
+        TeamData td = new TeamData();
+        InputStream in = new FileInputStream("PlayerPersonalData2.0.csv");
+        InputStreamReader playerStream = new InputStreamReader(in);
+        InputStream in1 = new FileInputStream("Team.csv");
+        InputStream in2 = new FileInputStream("Team_Attributes.csv");
+        InputStreamReader teamStream = new InputStreamReader(in1);
+        InputStreamReader teamAttrStream = new InputStreamReader(in2);
+        FifaDataController fdc = new FifaDataController(pd,td,playerStream,teamStream,teamAttrStream);
+        fdc.startReading();
+        assertTrue(pd.getPlayerList()!=null&&td.getTeamList()!=null);
     }
 
 }
